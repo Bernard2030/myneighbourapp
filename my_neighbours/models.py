31444a70f32ecdb5d2974be2c79c25abf902308f
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-30 00:03:00 +0300

    add neighbourhood model

--- my_neighbours/models.py
+++ my_neighbours/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Neighbourhood(models.Model):
+    neighbourhood = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.neighbourhood
+
+    def create_neighbourhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighbourhood(cls,neighbourhood):
+        cls.objects.filter(neighbourhood=neighbourhood).delete()       
+
+class Profile(models.Model):
+    

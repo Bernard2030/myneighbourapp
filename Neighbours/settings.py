6072fbf1ee778ff1920bc74cd7eab63dfdf3cba4
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-29 17:47:15 +0300

    add database

--- Neighbours/settings.py
+++ Neighbours/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'Neighbours.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neighbour',
+        'USER': 'moringa',
+    'PASSWORD':'Access',
     }
 }
 

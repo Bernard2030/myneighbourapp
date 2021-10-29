4917fbdfc6b7d424036fed55ca03b645dc58fa56
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-30 00:37:31 +0300

    add models for post profile and neighbourhood in models file

--- my_neighbours/models.py
+++ my_neighbours/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from tinymce.models import HTMLField
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -14,7 +16,30 @@ class Neighbourhood(models.Model):
 
     @classmethod
     def delete_neighbourhood(cls,neighbourhood):
-        cls.objects.filter(neighbourhood=neighbourhood).delete()       
+        neighbour_hood=cls.objects.filter(neighbourhood=neighbourhood).delete()  
+        return  neighbour_hood    
 
 class Profile(models.Model):
+    profile_pic = CloudinaryField('image')
+    neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+    description = HTMLField()
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
     image = CloudinaryField('image')
+    title = models.CharField(max_length=100)
+    post = HTMLField()
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    profile_pic = CloudinaryField('image')
+
+    def __str__(self):
+        return self.title
+
+        

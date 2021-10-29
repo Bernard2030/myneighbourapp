f2349f6e8f520979e4e0b40b0e814a206af48350
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-29 23:36:41 +0300

    add search html

--- my_neighbours/urls.py
+++ my_neighbours/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.Welcome, name='welcome'),
+    path('search/', views.search_results, name='search_results')
 ]
--- my_neighbours/views.py
+++ my_neighbours/views.py
@@ -10,3 +10,17 @@ def Welcome(request):
     return render(request, 'welcome.html')
 
 
+# def search_results(request):
+
+#     if 'article' in request.GET and request.GET["article"]:
+#         search_term = request.GET.get("article")
+#         searched_articles = Article.search_by_title(search_term)
+#         message = f"{search_term}"
+
+#         return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+
+#     else:
+#         message = "You haven't searched for any term"
+#         return render(request, 'all-news/search.html',{"message":message})    
+
+

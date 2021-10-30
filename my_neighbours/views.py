8611623d673c06e3077801518dcfc8210ed0a659
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-30 15:17:25 +0300

    add create profile function in views

--- my_neighbours/views.py
+++ my_neighbours/views.py
@@ -1,8 +1,10 @@
 from django.core.exceptions import ObjectDoesNotExist
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-from .forms import ProfileForm,BusinessForm,CommentsForm,PostForm
-from django.http import HttpResponse, Http404,HttpResponseRedirect
+
+from my_neighbours.forms import ProfileForm
+
 
 from .models import Profile,Post,Health,User
 
@@ -25,31 +27,32 @@ def Welcome(request):
 def my_profile(request):
     current_user=request.user
     profile=Profile.objects.get(username=current_user)
-    return render(request,'user_profile.html',{"profile":profile}) 
-
-# @login_required(login_url='/accounts/login/')
-# def user_profile(request,username):
-#     user = User.objects.get(username=username)
-#     profile = Profile.objects.get(username=user) 
+    return render(request,'user_profile.html',{'profile':profile})
 
 
-
+@login_required(login_url='/accounts/login/')
+def user_profile(request,username):
+    user = User.objects.get(username=username)
+    profile = Profile.objects.get(username=user)
 
 
 
-# def search_results(request):
+@login_required(login_url='/accounts/login/')
+def create_profile(request):
+    current_user = request.user
+    if request.method=="POST":
+        form = ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            profile = form.save(commit = False)
+            profile.username = current_user
+            profile.save()
+        return HttpResponseRedirect('/')
+    else:
+        form = ProfileForm()
+        return render(request,'profile_form.html',{"form":form})                 
 
-#     if 'article' in request.GET and request.GET["article"]:
-#         search_term = request.GET.get("article")
-#         searched_articles = Article.search_by_title(search_term)
-#         message = f"{search_term}"
+ 
 
-#         return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
-
-#     else:
-#         message = "You haven't searched for any term"
-#         return render(request, 'all-news/search.html',{"message":message})  
-# 
 
 
 

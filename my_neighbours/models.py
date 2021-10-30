5d1b49c8738ef68fc8c15032da3f48b1d70c08b2
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-30 10:01:53 +0300

    update models by adding health, comments, business and authorities models

--- my_neighbours/models.py
+++ my_neighbours/models.py
@@ -42,4 +42,62 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-        
+class Comments(models.Model):
+    comments = models.CharField(max_length=255)
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+
+
+class Business(models.Model):
+    logo = CloudinaryField('image')
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    email = models.EmailField()
+    contact = models.IntegerField()
+
+    def __str__(self):
+        return self.name   
+
+    @classmethod
+    def search_business(cls,search_term):
+        business = cls.objects.filter(description_icontains=search_term)
+        return business
+
+
+class Healthservices(models.Model):
+    healthservices = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.healthservices 
+
+    def save_healthservices(self):
+        self.save()
+
+    @classmethod
+    def delete_healthservices(cls,healthservices):
+        cls.objects.filter(healthservices=healthservices).delete()
+
+class Health(models.Model):
+    logo = CloudinaryField('image')
+    neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+    email = models.EmailField()
+    address = models.CharField(max_length=120)
+    contact = models.IntegerField()
+    healthservices = models.ManyToManyField(Healthservices)
+
+    def __str__(self):
+        return self.name
+
+
+class Authorities(models.Model):
+    neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+    email = models.EmailField()
+    address = models.CharField(max_length=120) 
+    contact = models.ImageField()
+
+    def __str__(self):
+        return self.name       

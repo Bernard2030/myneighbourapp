a737cd05c2862378b6e674529f79069c004341e7
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-30 14:14:30 +0300

    change welcome html to index html and add health and authorities url

--- my_neighbours/views.py
+++ my_neighbours/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from .forms import ProfileForm,BusinessForm,CommentsForm,PostForm
 from django.http import HttpResponse, Http404,HttpResponseRedirect
 
-from .models import Profile,Post,Health
+from .models import Profile,Post,Health,User
 
 
 
@@ -16,16 +16,22 @@ def Welcome(request):
         if not request.user.is_authenticated:
             return redirect('/account/login/')
         current_user=request.user
-        profile =Profile.objects.get(username=current_user)
+        # profile =Profile.objects.get(username=current_user)
     except ObjectDoesNotExist:
         return redirect('create_profile')
-    return render(request, 'welcome.html')
+    return render(request, 'index.html')
 
 @login_required(login_url='/accounts/login/')
 def my_profile(request):
     current_user=request.user
     profile=Profile.objects.get(username=current_user)
-    return render(request,'profile/profile.html',{"profile":Profile}) 
+    return render(request,'user_profile.html',{"profile":profile}) 
+
+# @login_required(login_url='/accounts/login/')
+# def user_profile(request,username):
+#     user = User.objects.get(username=username)
+#     profile = Profile.objects.get(username=user) 
+
 
 
 

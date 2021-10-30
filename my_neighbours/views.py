e57cdf602796c269de18b61d14039dc60f5263ae
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-30 16:32:56 +0300

    add post ,see post function in views

--- my_neighbours/views.py
+++ my_neighbours/views.py
@@ -3,10 +3,10 @@ from django.http.response import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 
-from my_neighbours.forms import ProfileForm
+from my_neighbours.forms import CommentsForm, ProfileForm
 
 
-from .models import Profile,Post,Health,User
+from .models import Comments, Profile,Post,Health,User
 
 
 
@@ -71,7 +71,46 @@ def updateP(request):
     else:
         form = ProfileForm()
 
-    return render(request,'update_profile.html',{"form":form})    
+    return render(request,'update_profile.html',{"form":form}) 
+
+
+
+@login_required(login_url='/accounts/login/')
+def my_post(request):
+    current_user = request.user
+    profile = Profile.objects.get(username=current_user)
+    posts = Post.objects.filter(neighbourhood=profile.neighbourhood)
+
+
+    return render(request,'my-posts.html',{'posts':posts}) 
+
+
+
+
+@login_required(login_url='/accounts/login/')
+def see_post(request,id):
+    current_user = request.user
+
+    try:
+        comments = Comments.objects.filter(post_id=id)
+    except:
+        comments =[]
+
+    my_post = Post.objects.get(id=id)
+    if request.method =='POST':
+        form = CommentsForm(request.POST,request.FILES)
+        if form.is_valid():
+            comments = form.save(commit=False)
+            comments.username = current_user
+            comments.post = my_post
+            comments.save()
+
+    else:
+        form = CommentsForm()
+    return render(request,'see-post.html',{"my_post":my_post,"form":form,"comments":comments})                           
+
+
+
 
 
 

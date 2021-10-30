f0d4b368dec82f7b331e941e40e44b928910b07d
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-30 16:57:27 +0300

    add my_post html file

--- my_neighbours/urls.py
+++ my_neighbours/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # path('search/', views.search_results, name='search_results')
     path('my-profile/',views.my_profile, name='my-profile'),
     path('create-profile/',views.create_profile, name='create-profile'),
-    path('profile_update/',views.updateP, name='profile_update')
+    path('profile_update/',views.updateP, name='profile_update'),
+    path('posts/',views.my_post, name='posts'),
 ]

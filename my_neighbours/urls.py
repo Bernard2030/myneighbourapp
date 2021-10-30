252692a8db5b8489abc5b52774990aa538d65a8a
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-30 16:09:15 +0300

    add update profile html

--- my_neighbours/urls.py
+++ my_neighbours/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',views.Welcome, name='welcome'),
     # path('search/', views.search_results, name='search_results')
     path('my-profile/',views.my_profile, name='my-profile'),
-    path('create-profile/',views.create_profile, name='create-profile')
+    path('create-profile/',views.create_profile, name='create-profile'),
+    path('profile_update/',views.updateP, name='profile_update')
 ]

6d003e7f3449e41e8ee94efa1ea58263917bb923
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-30 15:45:04 +0300

    add update profile in views

--- my_neighbours/views.py
+++ my_neighbours/views.py
@@ -49,7 +49,38 @@ def create_profile(request):
         return HttpResponseRedirect('/')
     else:
         form = ProfileForm()
-        return render(request,'profile.html',{"form":form})                 
+        return render(request,'profile.html',{"form":form}) 
+
+
+@login_required(login_url='/accounts/login/')
+def updateP(request):
+    current_user = request.user
+    if request.method=="POSt":
+        instance = Profile.objects.get(username=current_user)
+        form = ProfileForm(request.POST,request.FILES,instance=instance)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.username = current_user
+            profile.save()
+
+        return redirect('Index')
+
+    elif Profile.objects.get(username=current_user):
+        profile = Profile.objects.get(username=current_user)
+        form = ProfileForm(instance=profile)
+    else:
+        form = ProfileForm()
+
+    return render(request,'update_profile.html',{"form":form})    
+
+
+
+
+
+
+
+
+
 
  
 

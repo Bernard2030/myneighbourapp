984c06ad39e7030e81dabce6d257fbc63f2ae8f7
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-30 14:47:42 +0300

    add user html file

--- my_neighbours/urls.py
+++ my_neighbours/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.Welcome, name='welcome'),
     # path('search/', views.search_results, name='search_results')
+    path('my-profile/',views.my_profile, name='my-profile'),
 ]

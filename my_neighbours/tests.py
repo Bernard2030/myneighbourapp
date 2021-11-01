116c17f34d4b1d465a3c0b6927cdb4fe4f28a262
Bernard Opiyo <brobernard.254@gmail.com>
2021-11-01 10:43:25 +0300

    add test class

--- my_neighbours/tests.py
+++ my_neighbours/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Neighbourhood
 
 # Create your tests here.
+
+class NeighbourhoodTestClass(TestCase):
+    def setUp(self):
+        self.ngumba = Neighbourhood(neighbourhood='ngumba')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.ngumba,Neighbourhood))
+
+    def tearDown(self):
+        Neighbourhood.objects.all().delete()
+
+    def test_save_method(self):
+        self.ngumba.save_neighbourhood()
+        hood = Neighbourhood.objects.all()
+        self.assertTrue(len(hood)>0)
+
+    def test_delete_method(self):
+        self.ngumba.delete_neighbourhood('ngumba')
+        hood = Neighbourhood.objects.all()
+        self.assertTrue(len(hood)==0)
